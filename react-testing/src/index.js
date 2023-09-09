54df5c26d83c1f1d0ef73e3b422b2191b416dc84
CallumOB <callumobrien2004@gmail.com>
2023-09-09 19:05:15 +0200

    Built new react app for book list

--- react-testing/src/index.js
+++ react-testing/src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Component names must begin with capital letter
 function ReactFunction() {
-    return <h1>Something...</h1>;
+    // Must return JSX (HTML item)
+    return (
+        <React.Fragment>
+            <div className="test">
+                <h3>Hello World</h3>
+                <ul>
+                    <li>
+                        <a href="#">Hello world</a>
+                    </li>
+                </ul>
+                <h2>Test</h2>
+            </div>
+        </React.Fragment>
+    );
 }
+
+// Can also use React.createElement, but this is long-winded.
+// function ReactFunction() {
+//     return React.createElement('h2', {}, 'Hello World');
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
